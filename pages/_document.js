f0d89809a7fd048eced40114687bcc492ab86253
inacio <inacionoisemaia@gmail.com>
2021-01-13 08:32:29 -0300

    meta tags og and description

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
           />
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width" />
+          <meta name="keywords"  content="site, freelancer, blog, portfolio, desenvolvimento web, html, css, react js, javascript, next js"/>
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
@@ -21,6 +22,15 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto"
             rel="stylesheet"
           />
+
+          {/* <!-- Open Graph  --> */}
+          <meta property="og:site_name" content="inacio sites"/>
+          <meta property="og:title" content="inacio sites" />
+          <meta property="og:description" content="Desenvolvedor Web Freelancer"/>
+          <meta property="og:image" content="/inaciosites.png"/>
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://inaciosites.com.br" />
+          <meta property="fb:app_id" content="100677955001722" />
         </Head>
         <body>
           <Main />
