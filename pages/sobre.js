3a951df8886415791fec32145edbd8c41b5115a6
inacio <inacionoisemaia@gmail.com>
2022-03-30 09:01:04 -0300

    modificações página sobre

--- pages/sobre.js
+++ pages/sobre.js
@@ -5,8 +5,7 @@ const Sobre = () => (
     
   <p className={styles.text}>Sou filho de Santarém -PA, descendente de nordestinos desbravadores da amazônia. Tenho sangue de boiadeiros, agrigultores e soldados da borracha.
   Minha família se estabeleceu na cidade de Manaus, onde obtive educação escolar, técnica e acadêmica. Criei gosto pela literatura e filosofia. Em minha adolescência fiz vários cursos técnicos onde criei apreço pelo raciocínio lógico, programação e tecnologia. Por fim, na academia, fiz o curso de Licenciatura em matemática.
-  Tive a experiência de dar aulas de matemática para o ensino fundamental na região do rio Arapiuns-PA.
-  Agora, por fim, encontrei-me na programação novamente e não canso de aumentar meus conhecimentos na área diariamente.</p>
+  Tive a experiência de dar aulas de matemática para o ensino fundamental na região do rio Arapiuns-PA e Mojuí dos Campos-PA.</p>
   </Layout>
 );
 
