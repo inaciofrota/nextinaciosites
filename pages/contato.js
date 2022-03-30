a9c45120870303250a59cf46ff7739df1392e4b6
inaciofrota <inacionoisemaia@gmail.com>
2022-03-30 09:47:19 -0300

    inclusão botão whatsapp teste

--- pages/contato.js
+++ pages/contato.js
@@ -1,8 +1,11 @@
 import Layout from "../components/Layout";
 
 const Contato = () => (
-  <Layout title="Contato" text="inacionoisemaia@gmail.com" sessionimage="/Contato.png">
-    
+  <Layout title="Contato" text="Mande uma mensagem" sessionimage="/Contato.png">
+    <div className={styles.children}>
+      <a href="https://web.whatsapp.com/send?phone=5593991315355" target="_blanck">Whatsapps</a>
+    </div>
+
   </Layout>
 );
 
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,25 +2,25 @@ import Layout from "../components/Layout";
 import styles from '../styles/Portfolio.module.css'
 import Iframe from 'react-iframe'
 const Portfolio = () => (
-  <Layout title="Portfólio" text="Sites e aplicações web já desenvolvidos" sessionimage="/Portfolio.png" >
+  <Layout title="Portfólio" text="Sites já desenvolvidos" sessionimage="/Portfolio.png" >
 
 
       <div className={styles.children}>
 
      
-      <div className={styles.session}>
-        <h3 className={styles.titleh3}>Sites</h3>
-        <div className={styles.site}>
-          <Iframe src="https://redencaorock.com.br" scrolling="no" height="200" width="300" title="redenção"></Iframe>
-          <a href="https://redencaorock.com.br" target="_blanck" >Rendenção
-          </a>
+        <div className={styles.session}>
+          <h3 className={styles.titleh3}>Sites</h3>
+          <div className={styles.site}>
+            <Iframe src="https://redencaorock.com.br" scrolling="no" height="200" width="300" title="redenção"></Iframe>
+            <a href="https://redencaorock.com.br" target="_blanck" >Rendenção
+            </a>
+          </div>
         </div>
-      </div>
     
-      <div className={styles.session}>
-        <h3 className={styles.titleh3}>GitHub</h3>
-        <p>Aplicações web</p>
-      </div>
+        <div className={styles.session}>
+          <h3 className={styles.titleh3}>GitHub</h3>
+          <p>Aplicações web</p>
+        </div>
       </div>
   </Layout>
 );
