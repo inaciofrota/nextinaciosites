6d88e27f92b0ac1cb3e3e5cb5f8440c899f39b31
inaciofrota <inacionoisemaia@gmail.com>
2022-03-30 11:18:08 -0300

    whatsapp contato correçao

--- pages/contato.js
+++ pages/contato.js
@@ -3,9 +3,9 @@ import styles from '../styles/Contato.module.css'
 
 
 const Contato = () => (
-  <Layout title="Contato" text="Escolha um aplicativo para contato" sessionimage="/Contato.png">
+  <Layout title="Contato" text="Escolha um aplicativo para contato abaixo" sessionimage="/Contato.png">
     <div className={styles.children}>
-      <a href="https://web.whatsapp.com/send?phone=5593991315355" target="_blanck">
+      <a href="https://wa.me/send?phone=5593991315355" target="_blanck">
          <img src="whatsapp-logo-icone.png" alt='whatsapp' width='60' height='60'/>
       </a>
     </div>
